3b7155f9d21725d8b3e6fbec88246764ee98e627
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-02 14:41:21 -0500

    finished and cleaned up

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,32 @@
    write your logic.
 */
 
-let hand = [];
 
-let A =
+function handValue(hand) {
+  var result = 0;
+  var aces = 0;
 
-  function handValue(hand) {
-
-
-
-
-    return;
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i] === "A") {
+      aces++;
+    }
+    else if (hand[i] === "J" || hand[i] === "Q" || hand[i] === "K") {
+      result += 10;
+    }
+    else {
+      result += +hand[i];
+    }
   }
+  for (var j = aces; j > 0; j--) {
+    if (result > (11 - j)) {
+      result += 1;
+    }
+    else {
+      result += 11;
+    }
+  }
+  return result;
+}
 
 
 /* -----  Hints ------
@@ -27,5 +42,6 @@ let A =
 1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
 K, Q, J ==> Worth 10
 A       ==> Worth 1 or 11
+Bust = 22
 
 */
